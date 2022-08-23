5db82a1c3da46d186be6b03dde8411a729610dbc
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-23 12:17:38 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    toDo(e.target.description.value)
+  })
 });
+
+function toDo(toDoItems){
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteItem)
+  btn.textContent = ' X'
+  li.textContent = toDoItems;
+  li.appendChild(btn)
+  console.log(li);
+  document.querySelector('#tasks').appendChild(li);
+}
+function deleteItem(e){
+  e.target.parentNode.remove()
+}
